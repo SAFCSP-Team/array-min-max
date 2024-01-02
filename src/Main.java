c271d7086b0d33d0aeff5a684904e69a48c580db
LamRAM <150258612+LamRAM@users.noreply.github.com>
2024-01-02 17:26:54 +0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        float[] nums = {10.5f, 36.0f, 20.7f, 26.2f, 10.5f, 23.3f, -41.2f, 53.01f, 11.7f, 1.5f, 6.66f, -17.f, 55f };
+
+        /* your code here */
+    }
+}
+
